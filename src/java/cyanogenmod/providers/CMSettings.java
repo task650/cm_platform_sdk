fe629fdfe098676d78ada7ffc2a3815a1c61ab92
STELIX <ssspinni@gmail.com>
2016-01-14 23:18:02 +0100

    cmsdk - Dotted circle battery (3/3)
    Change-Id: Icf9aa7a6113a2428de4c2de9c73adc695b355e6e

--- src/java/cyanogenmod/providers/CMSettings.java
+++ src/java/cyanogenmod/providers/CMSettings.java
@@ -837,7 +837,7 @@ public final class CMSettings {
 
         /** @hide */
         public static final Validator STATUS_BAR_BATTERY_STYLE_VALIDATOR =
-                new DiscreteValueValidator(new String[] {"0", "2", "4", "5", "6"});
+                new DiscreteValueValidator(new String[] {"0", "2", "3", "4", "5", "6"});
 
         /**
          * Status bar battery %
