64768996803b9a56e4b23026491dccd465a0d5c0
STELIX <ssspinni@gmail.com>
2016-03-06 23:01:45 +0100

    Revert " CMSDK: Themes  Tile (2/2)"
    This reverts commit 6b8a2d8e5635cd9983d40c51a5acad48297cd903.

--- src/java/org/cyanogenmod/internal/logging/CMMetricsLogger.java
+++ src/java/org/cyanogenmod/internal/logging/CMMetricsLogger.java
@@ -66,6 +66,4 @@ public class CMMetricsLogger extends MetricsLogger {
     public static final int TILE_HEADS_UP = BASE + 39;
     public static final int TILE_BATTERY_SAVER = BASE + 40;
     public static final int TILE_CAFFEINE = BASE + 41;
-    public static final int TILE_THEMES = BASE + 42;
-
 }
